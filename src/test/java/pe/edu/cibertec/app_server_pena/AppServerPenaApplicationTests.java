6f0f85482506cfcfa1ff180abd330a5dac97826d
graldineps <geriifujioka@gmail.com>
2024-10-12 06:33:01 -0500

    Servidor spring cloud y eureka

---
+++ src/test/java/pe/edu/cibertec/app_server_pena/AppServerPenaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_server_pena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerPenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
